4b4ccb2a504434cf436e17b8594345853d8788f5
Yash Gohil <ygohil2350@gmail.com>
2022-01-24 16:40:49 +0530

    main

--- React/first/src/App.js
+++ React/first/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import"./my.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Row,Col,Card,Form } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Row and column</h1>
+      <Row>
+        <Col>
+        <Form>
+          <Form.Control Placeholder="Pationt Name" className="m-2"/>
+          <Form.Control Placeholder="Pationt city" className="m-2"/>
+          <Form.Control Placeholder="Pationt ID" className="m-2"/>
+          <Button className="m-2">Submit</Button>
+          <Button>Reset</Button>
+        </Form>
+        </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card><Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+      </Col>
+      </Row>
     </div>
   );
 }
